87e62800e74deefdfa72fc68cfa4c2796c2ca907
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-28 11:13:45 -0400

    updates

--- app_auth/views.py
+++ app_auth/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
